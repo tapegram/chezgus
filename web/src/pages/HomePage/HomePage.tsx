e3190249d5c134659361275cc4fe3fca8b000915
tavish <tapegram@gmail.com>
2022-07-04 12:46:19 -0400

    Add centered image

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -14,7 +14,15 @@ const HomePage = () => {
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
-      <img src="/img/housefront.jpg" alt="House"/>
+      <div className="w-full p-4">
+        <main role="main" className="w-full flex flex-col h-screen content-center justify-center">
+          <div className="w-full sm:w-1/2 lg:w-1/3 bg-gray-50 rounded-xl m-auto">
+            <div className="bg-white rounded shadow px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <img src="/img/housefront.jpg" alt="House" />
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
